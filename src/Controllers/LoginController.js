f13d6b70226b0cd7e188b83c5d2aa33aa8fb2c2e
Adriano de Paula Veríssimo Júnior <adriano753@live.com>
2020-03-27 20:03:32 -0300

    its work jwt

--- src/Controllers/LoginController.js
+++ src/Controllers/LoginController.js
@@ -4,9 +4,12 @@ const jwt = require('jsonwebtoken')
 module.exports = {
     async geraToken(request, response) {
         let { nome, senha } = request.body
-        const usuarioRetorno = await usuario.findOne({ Nome: nome })
-        // return response.json(usuarioRetorno._id)
-        const token = jwt.sign({ _id: usuarioRetorno._id }, process.env.JWT_KEY, { expiresIn: 300 })
-        return response.send({ auth: true, token: token })
+        const usuarioRetorno = await usuario.findOne({ Nome: nome, Senha: senha })
+        if (usuarioRetorno === null)
+            return response.send({ erro: "Usuário não encontrado" })
+        else {
+            const token = jwt.sign({ _id: usuarioRetorno._id }, process.env.JWT_KEY, { expiresIn: 300 });
+            return response.send({ auth: true, token: token });
+        }
     }
 }
